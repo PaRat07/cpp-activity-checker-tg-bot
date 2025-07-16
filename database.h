5732b4af1018947519ac7087e48eed5c44fbe009
PaRat07 <pasha.at.phone@gmail.com>
2025-07-16 23:20:34 +0300

    inline mode

--- database.h
+++ database.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <sqlite3.h>
 #include <filesystem>
 #include <string>
 #include <map>
@@ -24,30 +24,30 @@ std::vector<T> GetQueryResAll(SQLite::Statement &que) {
   std::vector<T> ans;
   while (que.executeStep()) {
     ans.emplace_back();
-    TemplateIndicies<std::tuple_size_v<T>>([&tup = ans.back(), &que] <size_t Ind> (std::index_sequence<size_t, Ind>) {
+    TemplateIndicies<std::tuple_size_v<T>>([&tup = ans.back(), &que] <size_t Ind> (std::integral_constant<size_t, Ind>) {
       std::get<Ind>(tup) = que.getColumn(Ind);
     });
   }
   return ans;
 }
 
-template<typename T> requires (requires { std::tuple_size_v<T>; })
-std::optional<T> GetQueryOne(SQLite::Statement &que) {
-  if (!que.executeStep()) {
-    return std::nullopt;
-  } else {
-    T ans;
-    TemplateIndicies<std::tuple_size_v<T>>([&tup = ans.back(), &que] <size_t Ind> (std::index_sequence<size_t, Ind>) {
-      std::get<Ind>(tup) = que.getColumn(Ind);
-    });
-    if (!que.executeStep()) {
-      assert(false);
-    }
-    return ans;
-  }
-}
+// template<typename T> requires (requires { typename std::tuple_size<T>; })
+// std::optional<T> GetQueryOne(SQLite::Statement &que) {
+//   if (!que.executeStep()) {
+//     return std::nullopt;
+//   } else {
+//     T ans;
+//     TemplateIndicies<std::tuple_size_v<T>>([&tup = ans.back(), &que] <size_t Ind> (std::integral_constant<size_t, Ind>) {
+//       std::get<Ind>(tup) = que.getColumn(Ind);
+//     });
+//     if (!que.executeStep()) {
+//       assert(false);
+//     }
+//     return ans;
+//   }
+// }
 
-template<typename T> requires (!requires { std::tuple_size_v<T>; })
+template<typename T>// requires (!requires { typename std::tuple_size<T>; })
 std::optional<T> GetQueryOne(SQLite::Statement &que) {
   if (!que.executeStep()) {
     return std::nullopt;
@@ -77,6 +77,32 @@ std::string EscapedStr(std::string_view sv) {
   ans.push_back('"');
   return ans;
 }
+static void sqliteLogCallback(void* /*pUserData*/, int errCode, const char* msg)
+{
+  // You can filter on errCode if you want only warnings, only errors, etc.
+  TGBM_LOG_DEBUG("[SQLite][code={}]: {}", errCode, msg);
+}
+
+static int traceCallback(
+    unsigned mask,
+    void*    /*pCtx*/,
+    void*    pStmt,
+    void*    /*pX*/
+){
+  auto* stmt = static_cast<sqlite3_stmt*>(pStmt);
+  const char* sql = sqlite3_sql(stmt);
+  if (sql) {
+    TGBM_LOG_DEBUG("[SQLite][code={}]: {}", mask, sql);
+
+    // This returns a NEW malloc’d string with parameters expanded
+    char* expanded = sqlite3_expanded_sql(stmt);
+    if (expanded) {
+    TGBM_LOG_DEBUG("[SQLite][code={}]: {}", mask, expanded);
+      sqlite3_free(expanded);
+    }
+  }
+  return 0;
+}
 
 class Database {
 private:
@@ -93,14 +119,25 @@ public:
   Database()
     : db([] -> SQLite::Database {
           fspath db_file = kDatabaseDir / "activity_checker_db.sq3";
+
+      // sqlite3_config(SQLITE_CONFIG_LOG, sqliteLogCallback, nullptr);
+
           auto db = SQLite::Database(db_file, SQLite::OPEN_CREATE | SQLite::OPEN_READWRITE);
-          db.exec("CREATE TABLE IF NOT EXIST activity (activity_check_id INTEGER, participant_id INTEGER)");
-          db.exec("CREATE TABLE IF NOT EXIST activity_check (id INTEGER AUTO_INCREMENT PRIMARY KEY, owner INTEGER)");
+
+          db.exec("PRAGMA journal_mode = WAL;");
+      sqlite3_trace_v2(
+     db.getHandle(),
+     SQLITE_TRACE_STMT,     // trace every statement
+     &traceCallback,
+     nullptr
+ );
+          db.exec("CREATE TABLE IF NOT EXISTS activity (activity_check_id INTEGER, participant_id INTEGER, UNIQUE(activity_check_id, participant_id))");
+          db.exec("CREATE TABLE IF NOT EXISTS activity_check (id INTEGER AUTO_INCREMENT PRIMARY KEY, owner INTEGER)");
           return db;
         } ()),
       add_activity_check_sttmnt(db, "INSERT OR IGNORE INTO activity_check (owner) VALUES (?)"),
       get_owner_by_id(db, "SELECT owner FROM activity_check WHERE id = ?"),
-      add_activity(db, "INSERT OR IGNORE INTO activity (activity_check_id, participant_id) (?, ?)"){
+      add_activity(db, "INSERT OR IGNORE INTO activity (activity_check_id, participant_id) VALUES (?, ?)"){
   }
 
   static constexpr std::string_view kActivityFileName = "acitity.csv";
@@ -146,7 +183,7 @@ public:
     };
     if (add_activity.exec() > 0) {
       fspath act_check_dir = kDatabaseDir / std::to_string(check_id);
-      std::string to_append = fmt::format(R"({},{},{:%Y-%m-%dT%H:%M:%S})", userId, EscapedStr(fullName), checkInDate);
+      std::string to_append = fmt::format("{},{},{:%Y-%m-%dT%H:%M:%S}\n", userId, EscapedStr(fullName), checkInDate);
       // TODO check if std::ofstream::write is atomic
       std::ofstream(act_check_dir / kActivityFileName, std::ios::app).write(to_append.data(), to_append.size());
       return "Спасибо, ваша активность учтена";
@@ -166,7 +203,7 @@ public:
     int64_t check_id = db.getLastInsertRowid();
     fspath act_check_dir = kDatabaseDir / std::to_string(check_id);
     std::filesystem::create_directory(act_check_dir);
-    static constexpr std::string_view kCsvHeaders = "userId,fullName,checkInDate";
+    static constexpr std::string_view kCsvHeaders = "userId,fullName,checkInDate\n";
     // TODO check if std::ofstream::write is atomic
     // std::ios::trunk is for cases, when
     std::ofstream(act_check_dir / kActivityFileName, std::ios::trunc).write(kCsvHeaders.data(), kCsvHeaders.size());
@@ -181,3 +218,12 @@ private:
   mutable SQLite::Statement get_owner_by_id;
   SQLite::Statement add_activity;
 };
+
+
+inline Database& GetDb() {
+  thread_local std::unique_ptr<Database> db;
+  if (!db) {
+    db = std::make_unique<Database>();
+  }
+  return *db;
+}
--- main.cpp
+++ main.cpp
@@ -9,41 +9,82 @@
 
 #include "database.h"
 
-static tgbm::api::reply_markup_t GetDataCollectorMessage() {
+#include <charconv>
+
+constexpr std::string_view kCallbackMarkActivePrefix = "mark_active ";
+
+static tgbm::api::InlineKeyboardMarkup GetDataCollectorMessage(int64_t id) {
   using namespace tgbm::api;
   using button = InlineKeyboardButton;
-  reply_markup_t markup;
+  InlineKeyboardMarkup markup;
 
-  auto& m = markup.emplace<InlineKeyboardMarkup>();
-  m.inline_keyboard = {
-      {{.text = "Отметить свою активность", .data = button::callback_data{.value = "mark_active"}}},
+  markup.inline_keyboard = {
+      {{.text = "Отметить", .data = button::callback_data{.value = fmt::format("mark_active {}", id)}}},
   };
   return markup;
+}
+
+std::optional<int64_t> ParseMarkActiveQuery(tgbm::api::optional<std::string> &query) {
+  if (query.has_value()) {
+    std::string_view str = query.value();
+    if (str.starts_with(kCallbackMarkActivePrefix)) {
+      int64_t ans;
+      if (std::from_chars(str.data() + kCallbackMarkActivePrefix.size() , str.data() + str.size(), ans).ec != std::errc()) {
+        return std::nullopt;
+      }
+      return ans;
+    } else {
+      return std::nullopt;
+    }
+  } else {
+    return std::nullopt;
+  }
 }
 
 static dd::task<void> answer_query(tgbm::bot& bot, tgbm::api::CallbackQuery q) {
   using namespace tgbm::api;
 
-  // if (q.data)
-
-  bool success = co_await bot.api.answerCallbackQuery({
-    .callback_query_id = q.id,
-    .text = q.data
-  });
-  if (!success) {
-    fmt::println(stderr, "bot cannot handle callback query =(");
+  int64_t act_check_id;
+  if (auto id_opt = ParseMarkActiveQuery(q.data); id_opt.has_value()) {
+    act_check_id = id_opt.value();
+  } else {
+    co_await bot.api.answerCallbackQuery({
+      .callback_query_id = q.id,
+        .text = fmt::format("Некорректный формат текста обратного вызова кнопки: \"{}\"", q.data.value_or("<empty>"))
+    });
     co_return;
   }
 
-  int_or_str id;
-  if (q.message)
-    id = q.message->chat->id;
+  std::string_view message = GetDb().AddActivity(act_check_id, q.from->id, fmt::format("{} {}", q.from->first_name, q.from->last_name.value_or("")), std::chrono::system_clock::now());
 
-  Message sended = co_await bot.api.sendMessage({
-      .chat_id = id,
-      .text = fmt::format("your answer is {}", q.data.value_or("<nothing>")),
+  bool success = co_await bot.api.answerCallbackQuery({
+    .callback_query_id = q.id,
+    .text = std::string(message)
   });
-  fmt::println("user receives: {}", sended);
+  if (!success) {
+    TGBM_LOG_ERROR("bot cannot handle callback query =(");
+    co_return;
+  }
+}
+
+dd::task<void> AnswerInlineQuery(tgbm::bot& bot, tgbm::api::InlineQuery q) {
+  using namespace tgbm::api;
+
+
+  int64_t act_id = GetDb().AddActivityCheck(q.from->id);
+  InlineQueryResultArticle res {
+    .id = "RESULT ID",
+      .title = fmt::format("Отметьте свою активность (ID проверки: )", act_id),
+      .description = "Description",
+    .input_message_content = InputTextMessageContent{ .message_text = "Message Text" }
+      // .reply_markup = GetDataCollectorMessage(act_id)
+  };
+
+  co_await bot.api.answerInlineQuery({
+    .inline_query_id = q.id,
+    .results = { InlineQueryResult{res} },
+    .is_personal = true,
+  });
 }
 
 dd::task<void> start_main_task(tgbm::bot& bot) {
@@ -54,9 +95,12 @@ dd::task<void> start_main_task(tgbm::bot& bot) {
   fmt::println("launching echobot, info: {}", co_await bot.api.getMe());
 
   co_foreach(tgbm::api::Update && u, bot.updates()) {
-    tgbm::api::CallbackQuery* cq = u.get_callback_query();
-    if (cq)
+
+    if (auto *cq = u.get_callback_query(); cq) {
       answer_query(bot, std::move(*cq)).start_and_detach();
+    } else if (auto *iq = u.get_inline_query(); iq) {
+      AnswerInlineQuery(bot, std::move(*iq)).start_and_detach();
+    }
   }
 }
 
@@ -66,25 +110,22 @@ int main() {
     fmt::println("launching telegram bot requires bot token from @BotFather");
     return -1;
   }
-  Database db;
 
   tgbm::bot bot{token /*"api.telegram.org", "some_ssl_certificate"*/};
 
-  bot.commands.add("get_keyboard", [&bot, &db](tgbm::api::Message&& m) {
-    std::invoke([&bot, m, &db] -> dd::task<void> {
-      db.AddActivityCheck();
+  bot.commands.add("get_keyboard", [bot_ptr = &bot](tgbm::api::Message&& m) {
+    std::invoke([] (tgbm::bot &bot, tgbm::api::Integer chat_id) -> dd::task<void> {
+      int64_t act_id = GetDb().AddActivityCheck(chat_id);
       co_await bot.api.sendMessage({
-        .chat_id = m.chat->id,
-        .text = fmt::format("ID этого опроса: {}")
+        .chat_id = chat_id,
+        .text = fmt::format("ID этой проверки активности: {}", act_id)
       });
-
-    }).start_and_detach();
-    dd::task sendmsg = bot.api.sendMessage({
-        .chat_id = m.chat->id,
-        .text = "Отметьте свою активность",
-        .reply_markup = GetDataCollectorMessage(),
-    });
-    sendmsg.start_and_detach();
+      co_await bot.api.sendMessage({
+        .chat_id = chat_id,
+        .reply_markup = GetDataCollectorMessage(act_id),
+        .text = "Отметьте свою активность"
+      });
+    }, *bot_ptr, m.chat->id).start_and_detach();
   });
 
   start_main_task(bot).start_and_detach();
